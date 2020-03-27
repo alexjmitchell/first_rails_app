5c54a3df0359cf5755eb1c9b6bc09b83afdbb454
Alex Mitchell <alexj.mitchell94@gmail.com>
2020-03-26 21:09:56 -0700

    added validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensitive: false },
+                       length: { minumum: 5, maximum: 25 }
+
+  VALID_EMAIL_REGEX = /\A([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
