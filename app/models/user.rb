5f9cdf376e609e6456bc853fffe31b31a34b4fa3
Alex Mitchell <alexj.mitchell94@gmail.com>
2020-04-02 20:56:10 -0700

    user password and security added

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false },
                     length: { maximum: 105 },
                     format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 8, maximum: 25 }
+  has_secure_password
 end
