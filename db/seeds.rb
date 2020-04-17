cf136165dd5ad4b75812f05a7bebc483dfbf2ce8
Alex Mitchell <alexj.mitchell94@gmail.com>
2020-04-17 13:26:20 -0700

    added pagination to several pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "Faker"
+
+5.times do
+  User.create(username: Faker::Internet.username, email: Faker::Internet.email, password: "password")
+end
